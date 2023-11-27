a6ea275dc235cde90a170b3780f47c726664280d
Oparechris <152171543+Oparechris@users.noreply.github.com>
2023-11-27 16:58:53 +0000

    update

---
+++ Course.js
@@ -0,0 +1,4 @@
+function enrollCourse(courseName) {
+    alert("You have enrolled in " + courseName);
+    // Additional enrollment logic can be added here, such as updating the user's profile or sending a confirmation email.
+}
